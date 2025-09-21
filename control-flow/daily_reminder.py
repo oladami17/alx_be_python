6e321c3190b41a28f413f83f52e46b47d48aca1d
Damilola Olatunji <odamilolaseun@outlook.com>
2025-09-21 19:57:35 +0100

    Updated the daily_reminder code

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -12,13 +12,13 @@ match priority:
     case "low":
         reminder = f"'{task}' is a low priority task"
     case _:
-        reminder = f"'{task}' is a unspecified priority"
+        reminder = f"'{task}' has an unspecified priority"
         # modify message if task is time-bound
 if time_bound == "yes":
     reminder += " that requires immediate attention today!"
 else:
     if priority == "low":
-        reminder = f"Note: {reminder}. Consider completing it when you have free time."
+        reminder = f"Note: {reminder}.Consider completing it when you have free time."
     else:
         reminder += " but it is not time-sensitive."
 print("\nReminder:", reminder)
